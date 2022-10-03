49d7ed88d824d35b0b172d56f7df51f032750605
henjus <80738089+henjus@users.noreply.github.com>
2022-10-03 21:59:25 +0300

    Tavoitesykelaskurin toteutus.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ika, setIka] = useState('')
+  const [sykerajat, setSykerajat] = useState('')
+
+  const laske = (e) => {
+    e.preventDefault()
+    const ylaraja = (220 - ika) * 0.85
+    const alaraja = (220 - ika) * 0.65
+    setSykerajat(parseInt(alaraja)+" - "+parseInt(ylaraja))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Sykkeen ylä- ja alaraja laskuri</h3>
+      <form onSubmit={laske}>
+        <div>
+          <label>Ikä</label> <br />
+          <input type="number" value={ika} onChange={e =>setIka(e.target.value)} />
+        </div>
+        <div>
+          <label>Sykerajat</label> <br />
+          <output>{sykerajat}</output>
+          {/* <input type="string" value={sykerajat} onChange={e =>setSykerajat(e.target.value)} /> */}
+        </div>
+        <button>Laske</button>
+      </form>
     </div>
   );
 }
